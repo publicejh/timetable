49d3f27979ef435b9d915b644f6cece74db9486c
publicejh <ejh@naver.com>
2019-11-01 09:26:57 +0900

    ADD lecture app

---
+++ timetable/lecture/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Lecture(models.Model):
+    code = models.CharField(max_length=32)
+    name = models.CharField(max_length=64)
+    professor = models.CharField(max_length=32)
+    location = models.CharField(max_length=32)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    day = models.ManyToManyField('lecture.DayOfWeek')
+
+    def __str__(self):
+        return self.name
+
+
+class DayOfWeek(models.Model):
+    name = models.CharField(max_length=4, unique=True)
+
+    def __str__(self):
+        return self.name
