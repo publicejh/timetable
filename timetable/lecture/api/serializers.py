9e2c5609a98094004d0ae73471b839f3aa3601bd
publicejh <ejh@naver.com>
2019-11-03 16:17:11 +0900

    ADD lecture add & remove

--- timetable/lecture/api/serializers.py
+++ timetable/lecture/api/serializers.py
@@ -4,6 +4,28 @@ from lecture.models import Lecture
 
 class LectureSerializer(serializers.ModelSerializer):
 
+    def validate_is_registered(self, data):
+
+        if data == False:
+            return data
+
+        selected_lecture_start_time = self.instance.start_time
+        selected_lecture_end_time = self.instance.end_time
+        selected_lecture_day = self.instance.day.all()
+
+        registered_lectures = Lecture.objects.filter(is_registered=True)
+
+        for registered_lecture in registered_lectures:
+            start_time = registered_lecture.start_time
+            end_time = registered_lecture.end_time
+            for day in registered_lecture.day.all():
+                for s_day in selected_lecture_day:
+                    if day == s_day:
+                        if not(selected_lecture_end_time <= start_time or end_time <= selected_lecture_start_time):
+                            raise serializers.ValidationError('겹치는 과목이 있습니다!')
+
+        return data
+
     class Meta:
         fields = '__all__'
         model = Lecture
--- timetable/lecture/static/lecture/action.js
+++ timetable/lecture/static/lecture/action.js
@@ -14,6 +14,7 @@ window.onload = function() {
         $('#modal-lecture-info .lecture-code span').text(code);
         $('#modal-lecture-info .lecture-professor span').text(professor);
         $('#modal-lecture-info .lecture-location span').text(location);
+        $('#modal-lecture-info #btn-add-lecture').data('id', id);
         $('#modal-lecture-info').modal('show');
     });
 
@@ -32,6 +33,7 @@ window.onload = function() {
         $('#modal-lecture-task .lecture-code span').text(code);
         $('#modal-lecture-task .lecture-professor span').text(professor);
         $('#modal-lecture-task .lecture-location span').text(location);
+        $('#modal-lecture-task #btn-remove-lecture').data('id', id);
         $('#modal-lecture-task').modal('show');
     });
 
@@ -50,4 +52,33 @@ window.onload = function() {
             }
         });
     });
+
+    $('#btn-add-lecture').click(function () {
+        let item = $(this);
+        let id = item.data("id");
+        $.ajax({
+          url: 'http://localhost:8003/api/v1/lectures/' + id,
+          type: 'PATCH',
+          data: "is_registered=true",
+          success: function(data) {
+            alert('과목을 등록했습니다.');
+            location.reload();
+          }
+        });
+    });
+
+    $('#btn-remove-lecture').click(function () {
+        let item = $(this);
+        let id = item.data("id");
+        $.ajax({
+          url: 'http://localhost:8003/api/v1/lectures/' + id,
+          type: 'PATCH',
+          data: "is_registered=false",
+          success: function(data) {
+            alert('과목을 제외했습니다.');
+            location.reload();
+          }
+        });
+    });
+
 };
